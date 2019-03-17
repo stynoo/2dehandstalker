4ecfba269fb00558d63a8e033e707b312d32d830
stynoo <no.spam@thankyoum.am>
2019-03-17 12:59:30 +0100

    fix default for pages argument

--- 2dehandstalker.py
+++ 2dehandstalker.py
@@ -6,39 +6,55 @@ from bs4 import BeautifulSoup
 
 
 def get_parsed_arguments():
-  parser = argparse.ArgumentParser(description='Get 2dehands items')
-  parser.add_argument('--filter', help='optional search filter')
-  parser.add_argument('--pages', help='optional amount of pages')
-  return parser.parse_args()
+    parser = argparse.ArgumentParser(description="Get 2dehands items")
+    parser.add_argument("--filter", help="optional search filter")
+    parser.add_argument(
+        "--pages", help="optional amount of pages (default: 1)", default=1
+    )
+    return parser.parse_args()
 
 
-def get_2dehands_items(query = None, pages = 1):
+def get_2dehands_items(query, pages):
     results = []
-
     for page in range(0, int(pages)):
-        offset = (38 * page)
-        page = requests.get(f'https://www.2dehands.be/markt/2/${query}/?offset=${offset}')
+        offset = 38 * page
+        page = requests.get(
+            f"https://www.2dehands.be/markt/2/${query}/?offset=${offset}"
+        )
         soup = BeautifulSoup(page.text, "html.parser")
-        
+
         if soup.find(class_="panel has-icon no-results"):
             return None
         elif soup.find(class_="search-result"):
             items = soup.find_all("article")
             for item in items:
                 item_json = {
-                    'name':     item.find(class_="listed-adv-item-link").contents[0].strip(),
-                    'date':     item.find(class_="listed-item-date").contents[0].strip(),
-                    'price':    item.find(class_="listed-item-price").contents[0].strip(),
-                    'url':      item.find(class_="listed-adv-item-link").get("href").strip(),
-                    'image':    item.find(class_="item-center-image").get("src").strip()
+                    "name": item.find(class_="listed-adv-item-link")
+                    .contents[0]
+                    .strip(),
+                    "date": item.find(class_="listed-item-date").contents[0].strip(),
+                    "price": item.find(class_="listed-item-price").contents[0].strip(),
+                    "url": item.find(class_="listed-adv-item-link").get("href").strip(),
+                    "image": item.find(class_="item-center-image").get("src").strip(),
                 }
                 if item_json:
                     results.append(item_json)
-        offset+=38
+        else:
+            print(
+                "Something went really wrong here: we found nothing, really, as in not a single thing...."
+            )
+            break
+        offset += 38
     return results
 
 
-if  __name__ == "__main__":
-   args = get_parsed_arguments()
-   print(json.dumps(get_2dehands_items(args.filter, args.pages), 
-                    sort_keys=True, indent=4, ensure_ascii=False))
+if __name__ == "__main__":
+    ARGS = get_parsed_arguments()
+    print(
+        json.dumps(
+            get_2dehands_items(ARGS.filter, ARGS.pages),
+            sort_keys=True,
+            indent=4,
+            ensure_ascii=False,
+        )
+    )
